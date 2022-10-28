6020486351394ef06f7ad6516337e5ccc399e298
yuka3vt <yukawardana587@gmail.com>
2022-10-28 13:54:33 +0700

    UTS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "ecommerce", component:EcommerceComponent
+  },
+  {
+    path: "", redirectTo: "ecommerce",pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    EcommerceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
